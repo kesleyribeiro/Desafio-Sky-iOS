00cbae6df41f94f79193cafdbfd8b074c03a0f73
Kesley Ribeiro <kesley.developer@icloud.com>
2018-06-07 23:08:27 -0300

    Update Model and UI

--- Sky Movies/Sky Movies/model/Movie.swift
+++ Sky Movies/Sky Movies/model/Movie.swift
@@ -18,6 +18,8 @@ class Movie {
     var _overview: String!
     var _duration: String!
     var _year: String!
+    var _image2: String!
+    var _image3: String!
     
     var title: String {
         if _title == nil {
@@ -53,6 +55,21 @@ class Movie {
         }
         return _year
     }
+    
+    var image2: String {
+        if _image2 == nil {
+            _image2 = ""
+        }
+        return _image2
+    }
+    
+    var image3: String {
+        if _image3 == nil {
+            _image3 = ""
+        }
+        return _image3
+    }
+
 
     init(movieDict: Dictionary<String, AnyObject>) {
 
@@ -75,6 +92,18 @@ class Movie {
         if let year = movieDict["release_year"] as? String {
             self._year = year
         }
+        
+        if let images = movieDict["backdrops_url"] as? [String] {
+            
+            if let image2 = images[0] as? String {
+                self._image2 = image2
+            }
+            
+            if let image3 = images[1] as? String {
+                self._image3 = image3
+            }
+        }
+
     }
 
 }
